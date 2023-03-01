4030e4c77f95a8321e83264987f8105872269b2d
thornsh <1183643226@qq.com>
2023-03-01 15:01:36 +0800

    first commit

---
+++ src/node/build.ts
@@ -0,0 +1,71 @@
+import { join } from 'path';
+import { build as viteBuild, InlineConfig} from 'vite'
+import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from './constants/index';
+import { RollupOutput } from 'rollup'
+import * as fs from 'fs-extra'
+
+export async function build(root: string = process.cwd()) {
+  const [clientBundle, serverBundle] = await bundle(root)
+
+  const serverEntryPath = join(root, '.temp', 'ssr-entry.js')
+  const { render } = require(serverEntryPath)
+
+  await renderPage(render, root, clientBundle)
+}
+
+export async function bundle(root: string) {
+  const resolveViteConfig = (isServer: boolean): InlineConfig => ({
+    mode: 'production',
+    root,
+    build: {
+      ssr: isServer,
+      outDir: isServer  ? '.temp' : 'build',
+      rollupOptions: {
+        input: isServer ? SERVER_ENTRY_PATH : CLIENT_ENTRY_PATH,
+        output: {
+          format: isServer ? 'cjs' : 'esm'
+        }
+      }
+    }
+  })
+
+  console.log(`Building client + server bundles...`)
+
+  try {
+    const [clientBundle, serverBundle] = await Promise.all([
+      viteBuild(resolveViteConfig(false)),
+      viteBuild(resolveViteConfig(true))
+    ])
+
+    return [clientBundle, serverBundle] as [RollupOutput, RollupOutput]
+  } catch(e) {
+
+  }
+}
+
+export async function renderPage(render: () => string, root: string, clientBundle: RollupOutput) {
+  const clientChunk = clientBundle.output.find(
+    (chunk) => chunk.type === 'chunk' && chunk.isEntry
+  )
+
+  console.log('rendering page in server side...')
+  const appHtml = render()
+  const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div id="root">${appHtml}</div>
+  <script type="module" src="/${clientChunk?.fileName}"></script>
+</body>
+</html>
+  `.trim()
+  await fs.ensureDir(join(root, 'build'))
+  await fs.writeFile(join(root, 'build/index.html'), html)
+  await fs.remove(join(root, '.temp'))
+}
---
+++ src/theme-default/Layout/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export function Layout() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>layout122</h1>
+      <div>
+        {count}
+        <button onClick={() => setCount(count + 1)}>add</button>
+      </div>
+    </div>
+  )
+}
